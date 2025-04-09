6f3a37db3892caa1f96299b7ee4ee91a7c80475f
anil-k-m <162057931+anil-k-m@users.noreply.github.com>
2025-04-09 00:09:38 -0400

    Time: 0 ms (100%), Space: 17.8 MB (51.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -16,23 +16,3 @@ class Solution:
         if not paranthesis_stack:
             return True
         return False
-        # paranthesis_count = {'(':0,'[':0,'{':0}
-        # for paranthesis in s:
-        #     if paranthesis == '(':
-        #         paranthesis_count['('] += 1
-        #     elif paranthesis == '[':
-        #         paranthesis_count['['] += 1
-        #     elif paranthesis == '{':
-        #         paranthesis_count['{'] += 1
-        #     elif paranthesis == ')':
-        #         paranthesis_count['('] -= 1
-        #     elif paranthesis == ']':
-        #         paranthesis_count['['] -= 1
-        #     elif paranthesis == '}':
-        #         paranthesis_count['{'] -= 1
-        #     if paranthesis_count['('] < 0 or paranthesis_count['['] < 0 or paranthesis_count['{'] < 0:
-        #         return False
-        # if paranthesis_count['('] == 0 and paranthesis_count['['] == 0 and paranthesis_count['{'] == 0:
-        #     return True
-        # return False
-        
