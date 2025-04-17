ef7d1111cab8bfe0fd7c3eed0ebbe8e2233f62af
anil-k-m <162057931+anil-k-m@users.noreply.github.com>
2025-04-17 13:11:44 -0400

    Time: 106 ms (47.93%), Space: 51.2 MB (92.18%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -3,11 +3,11 @@ class Solution:
         points.sort(key=lambda x:x[0])
         arrows = 1
         current_arrow = points[0][1]
-        for balloon in points[1:]:
-            if balloon[0] > current_arrow:
+        for i in range(1,len(points)):
+            if points[i][0] > current_arrow:
                 arrows += 1
-                current_arrow = balloon[1]
+                current_arrow = points[i][1]
             else:
-                current_arrow = min(balloon[1], current_arrow)
+                current_arrow = min(points[i][1], current_arrow)
         return arrows
             
